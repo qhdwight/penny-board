ab78f0d7bccdf84b6a1e2de505a3be45b588a566
qhdwight <qhdwight@gmail.com>
2020-03-04 09:42:47 -0800

    Motor controller

--- Penny Board/Program.cs
+++ Penny Board/Program.cs
@@ -1,18 +1,28 @@
 ﻿using System.Threading;
-using Microsoft.SPOT;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.MotorControl.CAN;
+using Microsoft.SPOT.Hardware;
+using Watchdog = CTRE.Phoenix.Watchdog;
 
 namespace Penny_Board
 {
     public static class Program
     {
+        private const int TimeoutMs = 150, UpdatePeriodMs = 20;
+        private static readonly VictorSPX Victor = new VictorSPX(0);
+        // private static readonly PWM Controller = new PWM(Cpu.PWMChannel.PWM_0, 0, 0, PWM.ScaleFactor.Microseconds, false);
+        private static readonly PWM Radio = new PWM(Cpu.PWMChannel.PWM_0, 10000, 1500, PWM.ScaleFactor.Microseconds, false);
+
         public static void Main()
         {
-            var counter = 0;
+            var configuration = new VictorSPXConfiguration();
+            Victor.ConfigAllSettings(configuration, TimeoutMs);
+            Victor.SetControlFramePeriod(ControlFrame.Control_3_General, UpdatePeriodMs);
+            Radio.Start();
             while (true)
             {
-                Debug.Print("Counter Value: " + counter);
-                ++counter;
-                Thread.Sleep(100);
+                Thread.Sleep(UpdatePeriodMs);
+                Watchdog.Feed();
             }
         }
     }
