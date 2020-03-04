b5752ded7a820453edc3b608918b9ba6ae7af1bc
qhdwight <qhdwight@gmail.com>
2020-03-03 23:30:12 -0800

    Add project files.

---
+++ Penny Board/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+
+namespace Penny_Board
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var counter = 0;
+            while (true)
+            {
+                Debug.Print("Counter Value: " + counter);
+                ++counter;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
